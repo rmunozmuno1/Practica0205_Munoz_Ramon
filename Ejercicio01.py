b70c3e417e2688167e1ab51937986ab433dcf366
rmunozmuno1 <rmunozmuno1@educacion.navarra.es>
2024-11-08 08:57:34 +0100

    Ejercicio1

---
+++ Ejercicio01.py
@@ -0,0 +1,7 @@
+### Escribir una función a la que se le pase una cadena <nombre> y muestre por pantalla el saludo ¡hola <nombre>!.
+def bienvenido(Nombre):
+    '''Definimos la funcion Bienvenido con Nombre'''
+    print ('!Hola', Nombre +'!')
+    '''Imprimimos en la función lo que queremos que aparezca'''
+bienvenido('Ramón')
+'''Usamos nuestra función'''
