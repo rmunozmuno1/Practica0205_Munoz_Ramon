380a6b9343b292f1adc8f814f9db9d2f5a09fa38
rmunozmuno1 <rmunozmuno1@educacion.navarra.es>
2024-11-08 09:53:19 +0100

    Ejercicio3

---
+++ Ejercicio03.py
@@ -0,0 +1,9 @@
+### Escribir una función que calcule el área de un círculo y otra que calcule el volumen de un cilindro usando la primera función.
+def Area(radio , pi):
+    Area_Resuelta = (pi * (radio ^ 2))
+    return Area_Resuelta
+print (Area(5,3.14))
+def Volumen_cilidro(altura):
+    print (Area * altura)
+    return Volumen_cilidro
+print (Volumen_cilidro(5))
