f374f5b38ea9cad9890b7c71c365474e7a4ae38e
rmunozmuno1 <rmunozmuno1@educacion.navarra.es>
2024-11-09 20:35:02 +0100

    Ejercicio2

--- Ejercicio02.py
+++ Ejercicio02.py
@@ -1,4 +1,5 @@
 ###Escribir una función que reciba un número entero positivo y devuelva su factorial. Realiza el ejercicio mediante bucles interactivos y mediante una función recursiva.
+###Recursiva
 import math
 import statistics
 def Factorial ():
@@ -7,3 +8,16 @@ def Factorial ():
     return factorial_nuevo
 '''Realizamos una función donde pedira al usuario meter un numero positivo y entero para que despues devuelva el factorial'''
 print (Factorial())
+###Interativo
+def Factorial2 ():
+    numero = int(input('Introduzca un número entero postivo para realizar el factorial'))
+    factorial2 = 1
+    while numero > 1:
+        facotial2 *= numero
+        numero -= 1
+    return factorial2
+    print (factorial2())
+print (Factorial2())
+   
+
+
